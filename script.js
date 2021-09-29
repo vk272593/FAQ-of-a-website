31bacbd3c609256d9b81c30021ec2f1744435217
vikash kumar <kumarvikash6101998@gmail.com>
2021-09-30 03:16:47 +0530

    using traversing the DOM

---
+++ script.js
@@ -0,0 +1,12 @@
+
+// Traversing the dom
+const btns=document.querySelectorAll('.question-btn');
+
+btns.forEach((btn)=>{
+    btn.addEventListener('click',(e)=>{
+        const question=e.currentTarget.parentElement.parentElement;
+        question.classList.toggle('show-text')
+    })
+})
+
+
