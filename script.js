18b9b2ece27adbbd1d13a12cb91c9ad187d16d21
vikash kumar <kumarvikash6101998@gmail.com>
2021-09-30 03:27:17 +0530

    using selector inside element

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 
 // Traversing the dom
-const btns=document.querySelectorAll('.question-btn');
+// const btns=document.querySelectorAll('.question-btn');
 
-btns.forEach((btn)=>{
-    btn.addEventListener('click',(e)=>{
-        const question=e.currentTarget.parentElement.parentElement;
+// btns.forEach((btn)=>{
+//     btn.addEventListener('click',(e)=>{
+//         const question=e.currentTarget.parentElement.parentElement;
+//         question.classList.toggle('show-text')
+//     })
+// })
+// using selector inside the element
+const questions=document.querySelectorAll('.question');
+questions.forEach((question)=>{
+    const btn=question.querySelector('.question-btn');
+    btn.addEventListener('click',()=>{
         question.classList.toggle('show-text')
     })
 })
